e672ea46009fb9bc518f1ed0d10211127c07424f
Vincent <asdfg10439l@gmail.com>
2021-03-25 15:05:56 +0800

    feat: add seeder

--- seeders/20210325011558-default-data.js
+++ seeders/20210325011558-default-data.js
@@ -1,7 +1,32 @@
 'use strict';
-
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    let userId = await queryInterface.bulkInsert('Users', [
+      {
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: '',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {})
+
+    await queryInterface.bulkInsert('Todos',
+      Array.from({ length: 10 }, (_, i) => {
+        return {
+          name: `name-${i}`,
+          UserId: userId,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      })
+      , {})
     /**
      * Add seed commands here.
      *
@@ -14,6 +39,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Todos', null, {})
+    await queryInterface.bulkDelete('Users', null, {})
     /**
      * Add commands to revert seed here.
      *
