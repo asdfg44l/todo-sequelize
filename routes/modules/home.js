af5105ab6147176f610e592f87917e60ad382b65
Vincent <asdfg10439l@gmail.com>
2021-03-26 14:51:37 +0800

    feat: add CRUD (use sequelize)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,9 +5,11 @@ const { Todo } = require('../../models')
 
 //首頁
 router.get('/', (req, res) => {
+  const user_id = req.user.id
   return Todo.findAll({
     raw: true,
-    nest: true
+    nest: true,
+    where: { UserId: user_id }
   })
     .then(todos => res.render('index', { todos }))
     .catch(err => console.log(err))
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,11 +3,77 @@ const router = express.Router()
 
 const { Todo } = require('../../models')
 
-router.get('/:id', (req, res) => {
+//new
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+//edit
+router.get('/:id/edit', async (req, res) => {
+  const user_id = req.user.id
   const todo_id = req.params.id
-  return Todo.findByPk(todo_id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(err => console.log(err))
+  try {
+    let todo = await Todo.findOne({ where: { id: todo_id, UserId: user_id } })
+    return res.render('edit', { todo: todo.toJSON() })
+  } catch (e) {
+    console.warn(e)
+  }
+})
+
+//create todo
+router.post('/', async (req, res) => {
+  const user_id = req.user.id
+  const { name } = req.body
+  try {
+    await Todo.create({ name, UserId: user_id })
+
+    return res.redirect('/')
+  } catch (e) {
+    console.warn(e)
+  }
+})
+
+//edit todo
+router.put('/:id', async (req, res) => {
+  const user_id = req.user.id
+  const todo_id = req.params.id
+  const { name, isDone } = req.body
+  try {
+    let todo = await Todo.findOne({ where: { id: todo_id, UserId: user_id } })
+    todo.name = name
+    todo.isDone = isDone === 'on'
+    await todo.save()
+    return res.redirect('/')
+  } catch (e) {
+    console.warn(e)
+  }
+})
+
+//delete todo
+router.delete('/:id', async (req, res) => {
+  const user_id = req.user.id
+  const todo_id = req.params.id
+  try {
+    let todo = await Todo.findOne({ where: { id: todo_id, UserId: user_id } })
+    await todo.destroy()
+    return res.redirect('/')
+  } catch (e) {
+    console.warn(e)
+  }
+})
+
+
+
+//detail
+router.get('/:id', async (req, res) => {
+  const user_id = req.user.id;
+  const todo_id = req.params.id
+  try {
+    let todo = await Todo.findOne({ where: { id: todo_id, UserId: user_id } })
+    return res.render('detail', { todo: todo.toJSON() })
+  } catch (e) {
+    console.warn(err)
+  }
 })
 
 module.exports = router
